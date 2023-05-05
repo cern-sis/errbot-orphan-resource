ca9bc1db99d0226b3cfb45ab1c7a47121975a399
Jimil Desai <jimildesai42@gmail.com>
2023-05-05 15:48:38 +0200

    add plugin to list orphan resources
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

---
+++ orphan.py
@@ -0,0 +1,26 @@
+from errbot import BotPlugin, botcmd
+from kubernetes import client, config
+
+class Orphan(BotPlugin):
+    
+    def activate(self):
+        super().activate()
+        config.load_incluster_config()
+
+    @botcmd
+    def orphan_resources(self, msg, args):
+        api = client.CoreV1Api()
+        namespaces = api.list_namespace().items
+        excluded_namespaces = ["jimil-test"]
+        resources = []
+        for ns in namespaces:
+            if ns.metadata.name not in excluded_namespaces:
+                api = client.CustomObjectsApi()
+                resources += api.list_cluster_custom_object(
+                        "argoproj.io", "v1alpha1", ns.metadata.name, "applications")["items"]
+        all_resources = api.list_cluster_custom_object("", "", "", "").get("items", [])
+        unmanaged_resources = []
+        for resource in all_resources:
+            if resource not in resources:
+                unmanaged_resources.append(resource)
+        return "\n".join([f"{r['kind']}/{r['metadata']['name']}" for r in unmanaged_resources])
