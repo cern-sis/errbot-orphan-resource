4904abd13f1605cf96a5e1abfa22f931e7168a64
Jimil Desai <jimildesai42@gmail.com>
2023-05-08 14:14:51 +0200

    use kwargs
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- orphan.py
+++ orphan.py
@@ -11,16 +11,20 @@ class Orphan(BotPlugin):
     def orphan_resources(self, msg, args):
         api = client.CoreV1Api()
         namespaces = api.list_namespace().items
-        self.log.info("namespaces: ")
-        self.log.info(namespaces)
+        # self.log.info("namespaces: ")
+        # self.log.info(namespaces)
         excluded_namespaces = ["jimil-test"]
         resources = []
         for ns in namespaces:
+            self.log.info("ns.metadata.name: ")
             self.log.info(ns.metadata.name)
             if ns.metadata.name not in excluded_namespaces:
                 api = client.CustomObjectsApi()
                 resources += api.list_cluster_custom_object(
-                    "argoproj.io", "v1alpha1", ns.metadata.name, "applications"
+                    group="argoproj.io",
+                    version="v1alpha1",
+                    namespace=ns.metadata.name,
+                    plural="applications",
                 )["items"]
         all_resources = api.list_cluster_custom_object("", "", "", "").get("items", [])
         unmanaged_resources = []
