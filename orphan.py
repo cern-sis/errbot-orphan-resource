5548126803e45faeba4712d08956a63a1537309e
Jimil Desai <jimildesai42@gmail.com>
2023-05-08 11:57:45 +0200

    add debug statement to print ns name
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- orphan.py
+++ orphan.py
@@ -1,8 +1,8 @@
 from errbot import BotPlugin, botcmd
 from kubernetes import client, config
 
+
 class Orphan(BotPlugin):
-    
     def activate(self):
         super().activate()
         config.load_incluster_config()
@@ -14,13 +14,17 @@ class Orphan(BotPlugin):
         excluded_namespaces = ["jimil-test"]
         resources = []
         for ns in namespaces:
+            print(ns.metadata.name)
             if ns.metadata.name not in excluded_namespaces:
                 api = client.CustomObjectsApi()
                 resources += api.list_cluster_custom_object(
-                        "argoproj.io", "v1alpha1", ns.metadata.name, "applications")["items"]
+                    "argoproj.io", "v1alpha1", ns.metadata.name, "applications"
+                )["items"]
         all_resources = api.list_cluster_custom_object("", "", "", "").get("items", [])
         unmanaged_resources = []
         for resource in all_resources:
             if resource not in resources:
                 unmanaged_resources.append(resource)
-        return "\n".join([f"{r['kind']}/{r['metadata']['name']}" for r in unmanaged_resources])
+        return "\n".join(
+            [f"{r['kind']}/{r['metadata']['name']}" for r in unmanaged_resources]
+        )
