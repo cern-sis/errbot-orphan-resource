28d2663ee5e5edb0466d04bdd7a5e2b3afb5555c
Jimil Desai <jimildesai42@gmail.com>
2023-05-09 09:26:42 +0200

    major refractor: list ALL the k8s objects not managed by argoCD
    Signed-off-by: Jimil Desai <jimildesai42@gmail.com>

--- orphan.py
+++ orphan.py
@@ -9,29 +9,56 @@ class Orphan(BotPlugin):
 
     @botcmd
     def orphan_resources(self, msg, args):
-        api = client.CoreV1Api()
-        namespaces = api.list_namespace().items
-        # self.log.info("namespaces: ")
-        # self.log.info(namespaces)
+        api = client.CustomObjectApi()
+        namespaces = client.CoreV1Api().list_namespace().items
+
+        argocd_group = "argoproj.io"
+        argocd_version = "v1alpha1"
+        argocd_plural = "applicationsets"
+
+        argocd_resources = []
         excluded_namespaces = ["jimil-test"]
-        resources = []
         for ns in namespaces:
             if ns.metadata.name not in excluded_namespaces:
-                api = client.CustomObjectsApi()
-                resources += api.list_cluster_custom_object(
-                    group="argoproj.io",
-                    version="v1alpha1",
-                    plural="applicationsets",
+                resources = api.list_namespaced_custom_object(
+                    group=argocd_group, version=argocd_version, plural=argocd_plural
                 )["items"]
-        all_resources = api.list_cluster_custom_object(
-            group="", version="v1", plural=""
-        ).get("items", [])
-        unmanaged_resources = [
-            resource for resource in all_resources if resource not in resources
+                argocd_resources += resources
+
+        k8s_resources = []
+        resource_types = [
+            "deployments",
+            "statefulsets",
+            "persistentvolumeclaims",
+            "services",
+            "cronjobs",
+            "jobs",
         ]
-        return "\n".join(
-            [
-                f"{r['kind']}/{r['metadata']['name']} ({r.get('metadata', {}).get('namespace')})"
-                for r in unmanaged_resources
-            ]
-        )
+        for resource_type in resource_types:
+            resources = getattr(client.AppsV1Api(), f"list_{resource_type}")().items
+            k8s_resources += resources
+
+        for resource_type in ["configmaps", "secrets"]:
+            resources = getattr(client.CoreV1Api(), f"list_{resource_type}")().items
+            k8s_resources += resources
+
+        unmanaged_resources = [
+            resource
+            for resource in k8s_resources
+            if (
+                not any(
+                    resource.metadata.name == argocd_res["metadata"]["name"]
+                    for argocd_res in argocd_resources
+                )
+                and resource.metadata.namespace not in excluded_namespaces
+            )
+        ]
+
+        output = [
+            f"{r.kind}/{r.metadata.name} ({r.metadata.namespace})"
+            for r in unmanaged_resources
+        ]
+
+        output_resource = "\n".join(output)
+
+        return output_resource
